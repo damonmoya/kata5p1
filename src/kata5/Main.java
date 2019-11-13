7dc49fa905931fe201763866b1aa316c1db4676d
Usuario <Usuario@10.22.148.96>
2019-11-13 09:48:45 +0000

    Conexión a BD (con errores de impresión)

--- src/kata5/Main.java
+++ src/kata5/Main.java
@@ -2,20 +2,24 @@ package kata5;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-
+    
     public static void main(String[] args) {
         String URL_BD_SQLite = new String("jdbc:sqlite:C:\\Users\\Usuario\\Documents\\NetBeansProjects\\DB_SQLite\\Prueba.db");
-        connect(URL_BD_SQLite);
+        Connection connection = connect(URL_BD_SQLite);
     }
 
-    private static void connect(String URL_BD_SQLite) {
+    private static Connection connect(String URL_BD_SQLite) {
         Connection connection = null;
         try{
             connection = DriverManager.getConnection(URL_BD_SQLite);
             System.out.println("Base de datos conectada...");
+            selectData_PEOPLE(connection);
+
         }
         catch (SQLException exception) {
             System.out.println("ERROR Kata5::connect (SQLException)" + exception.getMessage());
@@ -29,6 +33,26 @@ public class Main {
                 System.out.println("ERROR Kata5::connect-finally (SQLException)" + exception.getMessage());
             }
         }
+        return connection;
+    }
+    
+    private static void selectData_PEOPLE(Connection connection) {
+        String SQL = "SELECT * FROM PEOPLE";
+        try{
+           Statement statement = connection.createStatement();
+           ResultSet resultset = statement.executeQuery(SQL); 
+           while (resultset.next()) {
+               System.out.println("Id \t Nombre \t Apellidos \t Departamento" + 
+                       resultset.getInt("Id") + " \t " + 
+                       resultset.getString("Name") + " \t " +
+                       resultset.getInt("Apellidos") + " \t " +
+                       resultset.getInt("Departamento"));
+           }
+        }
+        catch (SQLException exception) {
+            System.out.println("ERROR Kata5::connect-finally (SQLException)" + exception.getMessage());
+        }
+        
     }
     
 }
